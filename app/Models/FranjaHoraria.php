9f764386589703df6ffcd521c56e5bbad057f718
miusarname2 <oscarmauricio631@gmail.com>
2025-04-02 23:43:30 -0500

    feat: :sparkles: Se añade la ultima migracion

---
+++ app/Models/FranjaHoraria.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FranjaHoraria extends Model
+{
+    use HasFactory;
+
+    protected $table = 'franja_horaria';
+    protected $primaryKey = 'idFranjaHoraria';
+
+    protected $fillable = [
+        'codigo',
+        'nombre',
+        'horaInicio',
+        'horaFin',
+        'duracionMinutos',
+        'tipo',
+        'estado'
+    ];
+    
+}
