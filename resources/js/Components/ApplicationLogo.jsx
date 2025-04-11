d4fcab58f4da77b5dda9e73849e598abbdfbc97e
miusarname2 <oscarmauricio631@gmail.com>
2025-04-11 16:18:56 -0500

    feat: :sparkles: Se añade la funcionalidad basica de redirreccionar al usuario al inicio al dar click ya sea en el nombre o ya sea en el logo

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -8,13 +8,15 @@ export default function ApplicationLogo({
   return (
     <div className="text-center md:text-left w-full max-w-md justify-center md:justify-start flex items-center">
       <div className="h-8 w-8 sm:h-10 sm:w-10 rounded-full bg-gradient-to-r from-blue-500 to-indigo-600 flex items-center justify-center mr-2 sm:mr-3">
-        <LayoutGrid className="w-4 h-4 sm:w-5 sm:h-5 text-white" />
+        <a href="/dashboard">
+          <LayoutGrid className="w-4 h-4 sm:w-5 sm:h-5 text-white" />
+        </a>
       </div>
-      <h2
+      <a href="/dashboard"><h2
         className={`${textSize} font-bold ${textColor}`}
       >
         PlanningProject
-      </h2>
+      </h2></a>
     </div>
   );
 }
