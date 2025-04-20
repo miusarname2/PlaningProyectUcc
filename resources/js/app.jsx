2806ae914189d1f34cbe9a383fc95fa63ce6226a
miusarname2 <oscarmauricio631@gmail.com>
2025-04-20 16:56:36 -0500

    feat: :sparkles: Se añadieron los toast en todo el proyecto

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,9 +3,11 @@ import '../css/app.css';
 
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
+import { Toaster } from '@/components/Toaster'
+import { ToastProvider } from '@radix-ui/react-toast'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Planing Project';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -13,7 +15,7 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(<ToastProvider swipeDirection="right"><App {...props} /> <Toaster /> </ToastProvider>);
     },
     progress: {
         color: '#4B5563',
