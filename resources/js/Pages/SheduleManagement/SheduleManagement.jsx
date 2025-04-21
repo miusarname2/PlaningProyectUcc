9308557853395a09f998032d53c31a51cb12fd8a
miusarname2 <oscarmauricio631@gmail.com>
2025-04-21 07:30:49 -0500

    feat: :sparkles: Se crea el metodo para exportar a xls

--- resources/js/Pages/SheduleManagement/SheduleManagement.jsx
+++ resources/js/Pages/SheduleManagement/SheduleManagement.jsx
@@ -1,16 +1,30 @@
 import ModulesLayout from "@/Layouts/ModulesLayout";
 import PrincipalShedule from "@/Pages/SheduleManagement/PrincipalShedule";
 import { Head } from "@inertiajs/react";
-import  HeaderSchedule  from "@/Components/HeaderSchedule"
+import HeaderSchedule from "@/Components/HeaderSchedule"
+import { getApi } from "@/utils/generalFunctions";
 
 
 export default function SheduleManagement({ auth }) {
+    const api = getApi();
+
+    const exportExcel = async () => {
+        const response = await api.get("/horario/export-xlsx");
+        const link = document.createElement('a');
+        link.href = 'data:application/vnd.ms-excel;base64,' + response.data.base64;
+        link.download = response.data.filename;
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+    }
+
     return (
         <ModulesLayout
             customHeader={
                 <HeaderSchedule
                     title="Cronograma de Horarios"
                     backTo="/dashboard"
+                    handleExport={exportExcel}
                 />
             }
         >
