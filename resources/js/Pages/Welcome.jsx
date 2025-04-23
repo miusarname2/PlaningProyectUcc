82682706dad51def3f3d3cb6a522bfc8420fe574
JuanDiegoQuinteroCampus <juanquinterocampu33@gmail.com>
2025-04-23 16:56:01 -0500

    feat: :art: prueba pullrequest  solicitado

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -31,7 +31,6 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                         </>
                     )}
                 </div>
-
                 <div className="max-w-7xl mx-auto p-6 lg:p-8">
                     <div className="flex justify-center">
                         <svg
@@ -46,7 +45,6 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                             />
                         </svg>
                     </div>
-
                     <div className="mt-16">
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                             <a
@@ -95,7 +93,6 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                     />
                                 </svg>
                             </a>
-
                             <a
                                 href="https://laracasts.com"
                                 className="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
