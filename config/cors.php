798e588ad89a0acb02105d8579be8dc3c862cf18
miusarname2 <oscarmauricio631@gmail.com>
2025-04-23 11:39:14 -0500

    feat: :rocket: cf

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '')),
+    'allowed_origins' => ['https://tu-app.laravel.cloud'],
 
     'allowed_origins_patterns' => [],
 
