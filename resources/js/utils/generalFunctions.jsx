804b264ba09903058cf95e8c602214814ff519f0
miusarname2 <oscarmauricio631@gmail.com>
2025-04-23 09:55:11 -0500

    feat: :rocket: Seploy config

--- resources/js/utils/generalFunctions.jsx
+++ resources/js/utils/generalFunctions.jsx
@@ -87,7 +87,7 @@ export async function decryptDataAESWithIV(encryptedHex, key) {
 
 export function createApiInstance() {
     const api = axios.create({
-        baseURL: "http://127.0.0.1:8000/api",
+        baseURL: "https://planingproyectucc-master-ag6e7l.laravel.cloud/api",
     });
 
     api.interceptors.request.use(async (config) => {
