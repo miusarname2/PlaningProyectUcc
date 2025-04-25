0e8e04c81919785230087d24ac73fe874a87214d
miusarname2 <oscarmauricio631@gmail.com>
2025-04-25 17:11:06 -0500

    feat: :sparkles:

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://planingproyectucc1-master-b16ilw.laravel.cloud','https://planingproyectucc-master-ag6e7l.laravel.cloud','103.133.1.1:443'],
+    'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => ['.laravel.cloud'],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
