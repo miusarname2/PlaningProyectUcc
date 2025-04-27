fbb1b7b25be963298d243f5a4f65f1a1d78e2516
miusarname2 <oscarmauricio631@gmail.com>
2025-04-26 21:04:50 -0500

    feat: :rotating_light: Se añade una constraint a los archivos de compilacion

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    esbuild: {
+        supported: {
+            'top-level-await': true
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
