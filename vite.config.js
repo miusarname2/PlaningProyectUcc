7cd369e4da40526c4a67658ee71a7021031ceb9c
miusarname2 <oscarmauricio631@gmail.com>
2025-05-01 14:50:55 -0500

    feat: :sparkles: Se migra a Laravel 12( inica la migracion)

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    esbuild: {
-        supported: {
-            'top-level-await': true
-        }
-    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
