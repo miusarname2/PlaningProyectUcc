09b61a77c96ead9e627da9cd0ebd5e5b15868caa
miusarname2 <oscarmauricio631@gmail.com>
2025-05-04 16:27:15 -0500

    feat: :sparkles: deleting route

--- routes/web.php
+++ routes/web.php
@@ -103,12 +103,6 @@ Route::get('locationManagement/department', function () {
     return Inertia::render('DepartamentManagement/DeparmentManagement');
 })->middleware(['auth', 'verified'])->name('DepartamentManagement');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
