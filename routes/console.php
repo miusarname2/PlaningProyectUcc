abef6a4ddd36cc715d24168c1bb81b27b4217638
miusarname2 <oscarmauricio631@gmail.com>
2025-05-12 16:32:31 -0500

    feat: :sparkles:

--- routes/console.php
+++ routes/console.php
@@ -9,5 +9,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('app:send-daily-dashboard-emails')
-        ->dailyAt('21:32')
+        ->dailyAt('21:35')
         ->withoutOverlapping();
