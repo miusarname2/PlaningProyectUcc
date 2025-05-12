43dbe04488dbffab3518b47223023b5712a8adf3
miusarname2 <oscarmauricio631@gmail.com>
2025-05-12 15:59:31 -0500

    feat: :sparkles: agendamiento

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('app:send-daily-dashboard-emails')
+        ->dailyAt('16:05')
+        ->withoutOverlapping();
