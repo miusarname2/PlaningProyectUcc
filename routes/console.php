cfd6febeed511f762f4cc29c348944ee1f434655
miusarname2 <oscarmauricio631@gmail.com>
2025-05-29 15:13:50 -0500

    feat: :rocket:

--- routes/console.php
+++ routes/console.php
@@ -9,6 +9,6 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('app:send-daily-dashboard-emails')
-    ->dailyAt('15:00')
+    ->dailyAt('15:17')
     ->timezone('America/Bogota')
     ->withoutOverlapping();
