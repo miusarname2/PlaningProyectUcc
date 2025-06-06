0146e17d8e6e6402944fa533e5845f88ddcc7320
miusarnameBotslovers <oscar.develop@botslovers.com>
2025-06-05 21:43:17 -0500

    feat: :bug: Se hacen cambios necesarios

--- database/migrations/2025_04_02_052856_create_usuario_rol_table.php
+++ database/migrations/2025_04_02_052856_create_usuario_rol_table.php
@@ -22,12 +22,7 @@ return new class extends Migration
         });
 
         DB::table('usuario_rol')->insert([
-            ['idUsuario' => 1, 'idRol' => 2],
-            ['idUsuario' => 2, 'idRol' => 2],
-            ['idUsuario' => 3, 'idRol' => 2],
-            ['idUsuario' => 4, 'idRol' => 2],
-            ['idUsuario' => 6, 'idRol' => 2],
-            ['idUsuario' => 5, 'idRol' => 5],
+            ['idUsuario' => 1, 'idRol' => 2]
         ]);
     }
 
