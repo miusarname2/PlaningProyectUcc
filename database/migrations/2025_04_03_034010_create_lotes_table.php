8e1945a0ddf0d1fdb9ded78b46f55150a9ad4e89
miusarnameBotslovers <oscar.develop@botslovers.com>
2025-06-10 22:14:44 -0500

    feat: :sparkles: Se hacen las modificaciones respectivas

--- database/migrations/2025_04_03_034010_create_lotes_table.php
+++ database/migrations/2025_04_03_034010_create_lotes_table.php
@@ -15,15 +15,15 @@ return new class extends Migration
             $table->increments('idLote');
             $table->string('codigo', 20)->unique();
             $table->string('nombre', 100);
-            $table->unsignedInteger('idPrograma')->nullable();
+            $table->unsignedInteger('idCiudad')->nullable();
             $table->date('fechaInicio');
             $table->date('fechaFin');
             $table->integer('numEstudiantes')->unsigned();
             $table->enum('estado', ['Activo', 'Inactivo']);
             $table->timestamps();
 
-            $table->foreign('idPrograma')
-                ->references('idPrograma')->on('programa')
+            $table->foreign('idCiudad')
+                ->references('idCiudad')->on('ciudad')
                 ->onDelete('set null');
         });
     }
